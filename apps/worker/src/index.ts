52002ee392d90389ef0fa8f1ecfdd0e10802b350
Esteban Sánchez <esteban.sanvaz@gmail.com>
2024-07-10 15:58:46 -0600

    job well done: kinda

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,5 +1,6 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import sharp from 'sharp';
 
 const readDirectory = async (dirPath: string): Promise<string[]> => {
   let filesList: string[] = [];
@@ -18,7 +19,7 @@ const readDirectory = async (dirPath: string): Promise<string[]> => {
 const deleteDirectory = async (dirPath: string): Promise<void> => {
   try {
     await fs.access(dirPath);
-    await fs.rmdir(dirPath, { recursive: true });
+    await fs.rm(dirPath, { recursive: true });
   } catch (error) {
     // Re-throw the error if it's not related to the existence of the directory
     if ((error as any).code !== 'ENOENT') throw error;
@@ -26,6 +27,7 @@ const deleteDirectory = async (dirPath: string): Promise<void> => {
 };
 
 const main = async () => {
+  console.log('Starting...');
   const assetsDir = path.join(__dirname, '..', 'assets', 'original');
   const resultsDir = path.join(__dirname, '..', 'assets', 'copy');
 
@@ -35,17 +37,29 @@ const main = async () => {
 
   const filePaths = await readDirectory(assetsDir);
 
+  const badPaths: string[] = [];
+
   for (const p of filePaths) {
     const relativePath = path.relative(assetsDir, p);
     const destination = path.join(resultsDir, relativePath);
 
-    if (path.extname(p) === '.svg') {
-      // TODO - convert svg to png using sharp
-    } else {
-      await fs.mkdir(path.dirname(destination), { recursive: true });
-      await fs.copyFile(p, destination);
+    try {
+      if (path.extname(p) === '.svg') {
+        const dest = destination.replace('.svg', '.png');
+        await fs.mkdir(path.dirname(dest), { recursive: true });
+        await sharp(p).png().toFile(destination.replace('.svg', '.png'));
+      } else {
+        await fs.mkdir(path.dirname(destination), { recursive: true });
+        await fs.copyFile(p, destination);
+      }
+    } catch (error) {
+      console.log(`Error: ${(error as any).message}`);
+      badPaths.push(p);
     }
   }
+
+  console.log(badPaths);
+  console.log('Done.');
 };
 
 main()
