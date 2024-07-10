525a009ed4908b02a615668d11f7593c1c78a15c
Esteban Sánchez <esteban.sanvaz@gmail.com>
2024-07-10 13:59:55 -0600

    set up

--- packages/ui/.storybook/theme.ts
+++
@@ -1,9 +0,0 @@
-import { create } from '@storybook/theming/create';
-
-export default create({
-  base: 'dark',
-  brandTitle: 'My BrandName',
-  brandUrl: 'https://github.com',
-  // brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
-  brandTarget: '_self',
-});
